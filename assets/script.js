250053ea9e1e1c02acdc035b7f266b1b6169090b
Ashley Zemina <amzemina@gmail.com>
2023-06-24 21:27:01 -0400

    added HTML, reset file, JS timer function

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var questions 
+var timerEl = document.getElementById("timer");
+var input
+
+
+function timer() {
+    var timeLeft = 5;
+  
+    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timerInterval = setInterval(function () {
+      
+      if(timeLeft >= 0) {
+        timerEl.textContent = "Timer: " + timeLeft;
+        console.log(timeLeft);
+        timeLeft--;
+      }          
+    }, 1000);
+  }
+  timer();
