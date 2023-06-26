ea46032adf0afb569b6d5e82fe7436e82042b864
Ashley Zemina <amzemina@gmail.com>
2023-06-25 22:29:25 -0400

    moved around var in js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,22 @@
-
-//Timer var and function
 var timerEl = document.getElementById("timer");
 var timeLeft = 60;
 var timerInterval;
+
 var scorePageEl = document.getElementById("scorepage")
 var leaderBoardEl = document.getElementById("leaderboard");
-
 var scoreEl = document.getElementById("score")
 
+var questionEL = document.getElementById("questions");
+var choiceAEL = document.getElementById("choiceA");
+var choiceBEL = document.getElementById("choiceB");
+var choiceCEL = document.getElementById("choiceC");
+var choiceDEL = document.getElementById("choiceD");
+var currentQuestion = -1;
+
+var introDivEl = document.getElementById("intro");
+var quizDivEl = document.getElementById("quiz");
+var initialsEl = document.getElementById("initials");
+var scoreListEl = document.getElementById("scorelist");
 
 
 function refTimer() {
@@ -15,92 +24,87 @@ function refTimer() {
 }
 
 function timer() {
-     timerInterval = setInterval(function () {
-      if(timeLeft > 0) {
-        timeLeft--;
-      } else {
-        endQuiz();
-      }    
-      refTimer();     
+    timerInterval = setInterval(function () {
+        if (timeLeft > 0) {
+            timeLeft--;
+        } else {
+            endQuiz();
+        }
+        refTimer();
     }, 1000);
-  }
+}
 
-  function deductTime() {
-    if (timeLeft -10 <= 0 ) {
+function deductTime() {
+    if (timeLeft - 10 <= 0) {
         timeLeft = 0
         endQuiz();
     } else {
-        timeLeft = timeLeft -10; 
+        timeLeft = timeLeft - 10;
     }
     refTimer();
-  }
+}
 
 
-  
+
 //Questions and function
 const questions = [
     {
-      question: "1. Which tag is used to define an unordered list in HTML?",
-      choices: ["<ol>", "<li>", "<ul>", "<dl>"],
-      answer: "<ul>"
+        question: "1. Which tag is used to define an unordered list in HTML?",
+        choices: ["<ol>", "<li>", "<ul>", "<dl>"],
+        answer: "<ul>"
     },
     {
-      question: "2. Which CSS property is used to change the text color of an element?",
-      choices: ["color", "text-color", "font-color", "text-style"],
-      answer: "color"
+        question: "2. Which CSS property is used to change the text color of an element?",
+        choices: ["color", "text-color", "font-color", "text-style"],
+        answer: "color"
     },
     {
-      question: "3. Which of the following is not a JavaScript data type?",
-      choices: ["Boolean", "String", "Number", "ArrayObject"],
-      answer: "ArrayObject"
+        question: "3. Which of the following is not a JavaScript data type?",
+        choices: ["Boolean", "String", "Number", "ArrayObject"],
+        answer: "ArrayObject"
     },
     {
-      question: "4. Which HTML tag is used to link an external JavaScript file?",
-      choices: ["<script>", "<link>", "<meta>", "<style>"],
-      answer: "<script>"
+        question: "4. Which HTML tag is used to link an external JavaScript file?",
+        choices: ["<script>", "<link>", "<meta>", "<style>"],
+        answer: "<script>"
     },
     {
-      question: "5. What does CSS stand for?",
-      choices: ["Cascading Style Sheet", "Computer Style Sheet", "Colorful Style Sheet", "Creative Style Sheet"],
-      answer: "Cascading Style Sheet"
+        question: "5. What does CSS stand for?",
+        choices: ["Cascading Style Sheet", "Computer Style Sheet", "Colorful Style Sheet", "Creative Style Sheet"],
+        answer: "Cascading Style Sheet"
     },
     {
-      question: "6. Which operator is used for strict equality comparison in JavaScript?",
-      choices: ["==", "===", "<=", "!=="],
-      answer: "==="
+        question: "6. Which operator is used for strict equality comparison in JavaScript?",
+        choices: ["==", "===", "<=", "!=="],
+        answer: "==="
     },
     {
-      question: "7. Which attribute is used to specify the source URL of an image in HTML?",
-      choices: ["src", "href", "link", "url"],
-      answer: "src"
+        question: "7. Which attribute is used to specify the source URL of an image in HTML?",
+        choices: ["src", "href", "link", "url"],
+        answer: "src"
     },
     {
-      question: "8. What is the correct syntax for a JavaScript comment?",
-      choices: ["// This is a comment", "<!-- This is a comment -->", "/* This is a comment */", "# This is a comment"],
-      answer: "// This is a comment"
+        question: "8. What is the correct syntax for a JavaScript comment?",
+        choices: ["// This is a comment", "<!-- This is a comment -->", "/* This is a comment */", "# This is a comment"],
+        answer: "// This is a comment"
     },
     {
-      question: "9. Which CSS property is used to control the spacing between lines of text?",
-      choices: ["text-spacing", "line-height", "text-line-spacing", "letter-spacing"],
-      answer: "line-height"
+        question: "9. Which CSS property is used to control the spacing between lines of text?",
+        choices: ["text-spacing", "line-height", "text-line-spacing", "letter-spacing"],
+        answer: "line-height"
     },
     {
-      question: "10. Which JavaScript method is used to remove the last element from an array?",
-      choices: [".pop()", ".push()", ".remove()", ".last()"],
-      answer: ".pop()"
+        question: "10. Which JavaScript method is used to remove the last element from an array?",
+        choices: [".pop()", ".push()", ".remove()", ".last()"],
+        answer: ".pop()"
     }
-  ];
+];
 
-  var questionEL = document.getElementById("questions");
-  var choiceAEL = document.getElementById("choiceA");
-  var choiceBEL = document.getElementById("choiceB");
-  var choiceCEL = document.getElementById("choiceC");
-  var choiceDEL = document.getElementById("choiceD");
-  var currentQuestion = -1;
 
-  function loadQ() {
+
+function loadQ() {
     currentQuestion++;
-    if (isCurrentQuestionValid()){
+    if (isCurrentQuestionValid()) {
         questionEL.textContent = questions[currentQuestion].question;
         choiceAEL.textContent = questions[currentQuestion].choices[0];
         choiceBEL.textContent = questions[currentQuestion].choices[1];
@@ -114,41 +118,36 @@ const questions = [
     } else {
         endQuiz();
     }
+}
 
-  }
-
-  function isCurrentQuestionValid() {
+function isCurrentQuestionValid() {
     if (currentQuestion >= questions.length) {
         return false;
     } else {
         return true;
     }
-  }
+}
 
-  function checkAns(chosenAns) {
+function checkAns(chosenAns) {
     if (chosenAns == questions[currentQuestion].answer) {
-        console.log("correct")
     } else {
         deductTime();
-        console.log("incorrect")
     };
     loadQ();
-  }
+}
 
 function endQuiz() {
     clearInterval(timerInterval);
     scoreEl.textContent = timeLeft;
-    console.log("quiz ended");
     scorePageEl.classList.remove("hidden");
     introDivEl.classList.add("hidden");
     quizDivEl.classList.add("hidden");
 }
-  
-//Start Quiz function
-var introDivEl = document.getElementById("intro");
-var quizDivEl = document.getElementById("quiz");
 
-  function startQuiz() {
+//Start Quiz function
+
+
+function startQuiz() {
     introDivEl.classList.add("hidden");
     quizDivEl.classList.remove("hidden");
     loadQ();
@@ -162,42 +161,42 @@ function showLeaderboard() {
     scorePageEl.classList.add("hidden");
     introDivEl.classList.add("hidden");
     quizDivEl.classList.add("hidden");
-    refreshScores() 
+    refreshScores()
 }
 
 function saveScores(allScores) {
-    allScores.sort((a,b)=>{
-        return b.score-a.score
+    allScores.sort((a, b) => {
+        return b.score - a.score
     });
     localStorage.setItem("allScores", JSON.stringify(allScores));
 
 }
 
-var initialsEl = document.getElementById("initials");
+
 function inputScore() {
     var allScores = getScores();
-    allScores.push( {initials: initialsEl.value, score: timeLeft});
+    allScores.push({ initials: initialsEl.value, score: timeLeft });
     saveScores(allScores);
     showLeaderboard()
 }
 
 function getScores() {
-  return JSON.parse(localStorage.getItem("allScores")) ||[];
+    return JSON.parse(localStorage.getItem("allScores")) || [];
 }
 
-var scoreListEl = document.getElementById("scorelist");
+
 function refreshScores() {
     var refAllScores = getScores()
     var delScoreList = scoreListEl.querySelectorAll("*")
-    
-    delScoreList.forEach(oldscore=>{
+
+    delScoreList.forEach(oldscore => {
         oldscore.remove();
     })
 
-    refAllScores.forEach(scoreEntry=>{
-      var entryContainerEl = document.createElement("div")
-      entryContainerEl.textContent = scoreEntry.initials + " " + scoreEntry.score;
-      scoreListEl.appendChild(entryContainerEl);
+    refAllScores.forEach(scoreEntry => {
+        var entryContainerEl = document.createElement("div")
+        entryContainerEl.textContent = scoreEntry.initials + " " + scoreEntry.score;
+        scoreListEl.appendChild(entryContainerEl);
     })
 }
 
@@ -207,6 +206,3 @@ function quizPage() {
     introDivEl.classList.remove("hidden");
     quizDivEl.classList.add("hidden");
 }
-
-
-console.log(getScores());
